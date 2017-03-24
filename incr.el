67c26de6384fa4b3d111b1aa7da509191f08e888
Colin Hogben <Colin.Hogben@ukaea.uk>
2017-03-24 16:25:14 +0000

    incr: Optional number to increment by

--- incr.el
+++ incr.el
@@ -2,13 +2,14 @@
 ;;	Increment number
 ;;-----------------------------------------------------------------------
 ;;;###autoload
-(defun increment-region (start end)
-  (interactive "r")
+(defun increment-region (start end &optional increment)
+  (interactive "r\nP")
   (let* ((old (buffer-substring start end))
 	 (iold (string-to-int old))
-	 (inew (1+ iold))
+	 (iinc (if (null increment) 1 increment))
+	 (inew (+ iold iinc))
 	 (new (int-to-string inew)))
-    (message "old[%s] new[%s]" old new)
+    (message "old[%s] new[%s] incr[%s]" old new increment)
     (delete-region start end)
     (goto-char start)
     (insert new)))
