d5899bcd03bf0a5ff2fd13bec3ae378f35cfac94
Colin Hogben <colin@pythontech.co.uk>
2022-10-07 20:38:40 +0100

    updateauto: Use make-directory-autoloads if available

--- updateauto.el
+++ updateauto.el
@@ -1,3 +1,7 @@
-(let ((generated-autoload-file
-       (expand-file-name (getenv "AUTOFILE"))))
-  (batch-update-autoloads))
+(let ((autofile (or (getenv "AUTOFILE") "ch-auto.el")))
+  (cond
+   ((fboundp 'make-directory-autoloads)
+    (make-directory-autoloads "." autofile))
+   (t
+    (let ((generated-autoload-file (expand-file-name autofile)))
+      (batch-update-autoloads)))))
